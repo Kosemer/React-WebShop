3f457029fd17ee6a7f6ae170ab7f0eeabb3f7593
Mokány Ádám <mokanyadam@gmail.com>
2023-07-26 00:16:19 +0200

    no message

--- src/Pages/DeliveryDetails.js
+++ src/Pages/DeliveryDetails.js
@@ -14,7 +14,7 @@ function DeliveryDetails() {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-
+  //
   const firebaseConfig = {
     apiKey: "AIzaSyA9I3pmY-2rE2GekX7A3angjr9GI8Gc-3U",
     authDomain: "webshopproducts-c1673.firebaseapp.com",
@@ -72,8 +72,6 @@ function DeliveryDetails() {
         selectedShippingMethod: cartCtx.selectedShippingMethod,
         selectedPaymentMethod: cartCtx.selectedPaymentMethod,
       })
-
-      
         .then(() => {
           cartCtx.setOrderId(newOrderRef.key); // Rendelés azonosító kiolvasása
           // Kosár tartalmának törlése
@@ -115,164 +113,112 @@ function DeliveryDetails() {
     <Fragment>
       {!cartCtx.isCartEmpty && (
         <div>
-      <div className={classes.progressBar}>
-        <StepProgressBar></StepProgressBar>
-      </div>
-      <section className={classes.products}>
-        <section className={classes.items}>
-          <Card>
-            <h2 className={classes.titleBox}>Kapcsolattartás</h2>
-            <div className={classes.fields}>
-              <div className={classes.inputBox}>
-                <label htmlFor="email">E-mail:</label>
-                <input
-                  type="email"
-                  id="email"
-                  name="email"
-                  value={cartCtx.formValues.email}
-                  onChange={handleInputChange}
-                ></input>
-                {continueClicked && cartCtx.formValues.email === "" && (
-                  <p className={classes.requiredField}>
-                    A mező kitöltése kötelező
-                  </p>
-                )}
-              </div>
-              <div className={classes.inputBox}>
-                <label htmlFor="phone">Telefonszám:</label>
-                <input
-                  type="text"
-                  id="phone"
-                  name="phone"
-                  value={cartCtx.formValues.phone}
-                  onChange={handleInputChange}
-                  className={classes.shortField}
-                ></input>
-                {continueClicked && cartCtx.formValues.phone === "" && (
-                  <p className={classes.requiredField}>
-                    A mező kitöltése kötelező
-                  </p>
-                )}
-              </div>
-            </div>
-            <h2 className={classes.titleBox}>Számlázási adatok</h2>
-            <div className={classes.fields}>
-              <div className={classes.inputBox}>
-                <label htmlFor="name">Név:</label>
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  size="60"
-                  value={cartCtx.formValues.name}
-                  onChange={handleInputChange}
-                ></input>
-                {continueClicked && cartCtx.formValues.name === "" && (
-                  <p className={classes.requiredField}>
-                    A mező kitöltése kötelező!
-                  </p>
-                )}
-              </div>
-              <div className={classes.inputBox}>
-                <label htmlFor="postalCode">Irányítószám:</label>
-                <input
-                  type="text"
-                  id="postalCode"
-                  name="postalCode"
-                  minLength="4"
-                  size="30"
-                  value={cartCtx.formValues.postalCode}
-                  onChange={handleInputChange}
-                  className={classes.shortField}
-                ></input>
-                {continueClicked && cartCtx.formValues.postalCode === "" && (
-                  <p className={classes.requiredField}>
-                    A mező kitöltése kötelező
-                  </p>
-                )}
-              </div>
-              <div className={classes.inputBox}>
-                <label htmlFor="city">Település:</label>
-                <input
-                  type="text"
-                  id="city"
-                  name="city"
-                  value={cartCtx.formValues.city}
-                  onChange={handleInputChange}
-                ></input>
-                {continueClicked && cartCtx.formValues.city === "" && (
-                  <p className={classes.requiredField}>
-                    A mező kitöltése kötelező
-                  </p>
-                )}
-              </div>
-              <div className={classes.inputBox}>
-                <label htmlFor="street">Utca, házszám:</label>
-                <input
-                  type="text"
-                  id="street"
-                  name="street"
-                  value={cartCtx.formValues.street}
-                  onChange={handleInputChange}
-                ></input>
-                {continueClicked && cartCtx.formValues.street === "" && (
-                  <p className={classes.requiredField}>
-                    A mező kitöltése kötelező
-                  </p>
-                )}
-              </div>
-              <div className={classes.inputBox}>
-                <label htmlFor="floor">Emelet, ajtó:</label>
-                <input
-                  type="text"
-                  id="floor"
-                  name="floor"
-                  className={classes.shortField}
-                  value={cartCtx.formValues.floor}
-                  onChange={handleInputChange}
-                ></input>
-                {continueClicked && cartCtx.formValues.floor === "" && (
-                  <p className={classes.requiredField}>
-                    A mező kitöltése kötelező
-                  </p>
-                )}
-              </div>
-              <div className={classes.inputBox}>
-                <label htmlFor="taxNumber">Adószám (ha van):</label>
-                <input type="text" id="taxNumber" name="taxNumber"></input>
-              </div>
-            </div>
-            <input
-              type="checkbox"
-              defaultChecked={true}
-              onClick={deliveryDetailsShowHandler}
-            ></input>
-            <p className={classes.deliveryCheckbox}>
-              A szállítási és a számlázási adatok megegyeznek.
-            </p>
-            {deliveryDetailsShow && (
-              <Fragment>
-                <h2 className={classes.titleBox}>Szállítási adatok</h2>
+          <div className={classes.progressBar}>
+            <StepProgressBar></StepProgressBar>
+          </div>
+          <section className={classes.products}>
+            <section className={classes.items}>
+              <Card>
+                <h2 className={classes.titleBox}>Kapcsolattartás</h2>
                 <div className={classes.fields}>
-                  <label htmlFor="name">Név:</label>
-                  <input type="text" id="name" name="name" size="60"></input>
                   <div className={classes.inputBox}>
-                    <label for="postalCode">Irányítószám:</label>
+                    <label htmlFor="email">E-mail:</label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      value={cartCtx.formValues.email}
+                      onChange={handleInputChange}
+                    ></input>
+                    {continueClicked && cartCtx.formValues.email === "" && (
+                      <p className={classes.requiredField}>
+                        A mező kitöltése kötelező
+                      </p>
+                    )}
+                  </div>
+                  <div className={classes.inputBox}>
+                    <label htmlFor="phone">Telefonszám:</label>
+                    <input
+                      type="text"
+                      id="phone"
+                      name="phone"
+                      value={cartCtx.formValues.phone}
+                      onChange={handleInputChange}
+                      className={classes.shortField}
+                    ></input>
+                    {continueClicked && cartCtx.formValues.phone === "" && (
+                      <p className={classes.requiredField}>
+                        A mező kitöltése kötelező
+                      </p>
+                    )}
+                  </div>
+                </div>
+                <h2 className={classes.titleBox}>Számlázási adatok</h2>
+                <div className={classes.fields}>
+                  <div className={classes.inputBox}>
+                    <label htmlFor="name">Név:</label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      size="60"
+                      value={cartCtx.formValues.name}
+                      onChange={handleInputChange}
+                    ></input>
+                    {continueClicked && cartCtx.formValues.name === "" && (
+                      <p className={classes.requiredField}>
+                        A mező kitöltése kötelező!
+                      </p>
+                    )}
+                  </div>
+                  <div className={classes.inputBox}>
+                    <label htmlFor="postalCode">Irányítószám:</label>
                     <input
                       type="text"
                       id="postalCode"
                       name="postalCode"
-                      minlength="4"
+                      minLength="4"
+                      size="30"
+                      value={cartCtx.formValues.postalCode}
+                      onChange={handleInputChange}
                       className={classes.shortField}
                     ></input>
+                    {continueClicked &&
+                      cartCtx.formValues.postalCode === "" && (
+                        <p className={classes.requiredField}>
+                          A mező kitöltése kötelező
+                        </p>
+                      )}
                   </div>
                   <div className={classes.inputBox}>
-                    <label htmlFor="town">Település:</label>
-                    <input type="text" id="town" name="town"></input>
+                    <label htmlFor="city">Település:</label>
+                    <input
+                      type="text"
+                      id="city"
+                      name="city"
+                      value={cartCtx.formValues.city}
+                      onChange={handleInputChange}
+                    ></input>
+                    {continueClicked && cartCtx.formValues.city === "" && (
+                      <p className={classes.requiredField}>
+                        A mező kitöltése kötelező
+                      </p>
+                    )}
                   </div>
                   <div className={classes.inputBox}>
                     <label htmlFor="street">Utca, házszám:</label>
-                    <input type="text" id="street" name="street"></input>
+                    <input
+                      type="text"
+                      id="street"
+                      name="street"
+                      value={cartCtx.formValues.street}
+                      onChange={handleInputChange}
+                    ></input>
+                    {continueClicked && cartCtx.formValues.street === "" && (
+                      <p className={classes.requiredField}>
+                        A mező kitöltése kötelező
+                      </p>
+                    )}
                   </div>
                   <div className={classes.inputBox}>
                     <label htmlFor="floor">Emelet, ajtó:</label>
@@ -281,39 +227,97 @@ function DeliveryDetails() {
                       id="floor"
                       name="floor"
                       className={classes.shortField}
+                      value={cartCtx.formValues.floor}
+                      onChange={handleInputChange}
                     ></input>
+                    {continueClicked && cartCtx.formValues.floor === "" && (
+                      <p className={classes.requiredField}>
+                        A mező kitöltése kötelező
+                      </p>
+                    )}
+                  </div>
+                  <div className={classes.inputBox}>
+                    <label htmlFor="taxNumber">Adószám (ha van):</label>
+                    <input type="text" id="taxNumber" name="taxNumber"></input>
                   </div>
                 </div>
-              </Fragment>
-            )}
-            <h2 className={classes.titleBox}>Megjegyzés</h2>
-            <div className={classes.fieldsComment}>
-              <label htmlFor="comment">
-                Ha bármilyen további információt szeretne közölni, akkor
-                használja az alábbi mezőt:
-              </label>
-              <textarea
-                rows="10"
-                cols="120"
-                id="comment"
-                name="comment"
-                type="text"
-              ></textarea>
-            </div>
-          </Card>
-        </section>
-        <section className={classes.amount}>
-          <OrderSummary
-            handleContinueClick={
-              shouldCallContinueClick ? handleContinueClick : null
-            }
-            buttonText={"Rendelés"}
-          ></OrderSummary>
-        </section>
-      </section>
-      </div>
-       )}
-       {cartCtx.isCartEmpty && <EmptyCart></EmptyCart>}
+                <input
+                  type="checkbox"
+                  defaultChecked={true}
+                  onClick={deliveryDetailsShowHandler}
+                ></input>
+                <p className={classes.deliveryCheckbox}>
+                  A szállítási és a számlázási adatok megegyeznek.
+                </p>
+                {deliveryDetailsShow && (
+                  <Fragment>
+                    <h2 className={classes.titleBox}>Szállítási adatok</h2>
+                    <div className={classes.fields}>
+                      <label htmlFor="name">Név:</label>
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        size="60"
+                      ></input>
+                      <div className={classes.inputBox}>
+                        <label for="postalCode">Irányítószám:</label>
+                        <input
+                          type="text"
+                          id="postalCode"
+                          name="postalCode"
+                          minlength="4"
+                          className={classes.shortField}
+                        ></input>
+                      </div>
+                      <div className={classes.inputBox}>
+                        <label htmlFor="town">Település:</label>
+                        <input type="text" id="town" name="town"></input>
+                      </div>
+                      <div className={classes.inputBox}>
+                        <label htmlFor="street">Utca, házszám:</label>
+                        <input type="text" id="street" name="street"></input>
+                      </div>
+                      <div className={classes.inputBox}>
+                        <label htmlFor="floor">Emelet, ajtó:</label>
+                        <input
+                          type="text"
+                          id="floor"
+                          name="floor"
+                          className={classes.shortField}
+                        ></input>
+                      </div>
+                    </div>
+                  </Fragment>
+                )}
+                <h2 className={classes.titleBox}>Megjegyzés</h2>
+                <div className={classes.fieldsComment}>
+                  <label htmlFor="comment">
+                    Ha bármilyen további információt szeretne közölni, akkor
+                    használja az alábbi mezőt:
+                  </label>
+                  <textarea
+                    rows="10"
+                    cols="120"
+                    id="comment"
+                    name="comment"
+                    type="text"
+                  ></textarea>
+                </div>
+              </Card>
+            </section>
+            <section className={classes.amount}>
+              <OrderSummary
+                handleContinueClick={
+                  shouldCallContinueClick ? handleContinueClick : null
+                }
+                buttonText={"Rendelés"}
+              ></OrderSummary>
+            </section>
+          </section>
+        </div>
+      )}
+      {cartCtx.isCartEmpty && <EmptyCart></EmptyCart>}
     </Fragment>
   );
 }
