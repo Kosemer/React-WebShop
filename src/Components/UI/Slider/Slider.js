948f3a986667e2b05ec49aa5060362ac1a2efb8b
Mokány Ádám <mokanyadam@gmail.com>
2023-07-28 00:04:00 +0200

    no message

--- src/Components/UI/Slider/Slider.js
+++ src/Components/UI/Slider/Slider.js
@@ -3,7 +3,7 @@ import "react-awesome-slider/dist/styles.css";
 import classes from "./Slider.module.css";
 import "react-awesome-slider/dist/custom-animations/fold-out-animation.css";
 import withAutoplay from "react-awesome-slider/dist/autoplay";
-import image from "../../../Assets/cooler3.png";
+import image from "../../../Assets/alienware.jpg";
 import image2 from "../../../Assets/iphone14pro.jpg";
 import image3 from "../../../Assets/macBookPro.jpg";
 
